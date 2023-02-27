97f33253d33fcef73d410fb80a8ea16385242667
Emre <emre.isbilir@gmail.com>
2023-02-27 15:28:11 +0300

    update sync service

--- src/main/java/com/topcoder/onlinereview/component/grpcclient/sync/SyncServiceRpc.java
+++ src/main/java/com/topcoder/onlinereview/component/grpcclient/sync/SyncServiceRpc.java
@@ -1,5 +1,6 @@
 package com.topcoder.onlinereview.component.grpcclient.sync;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,29 +27,53 @@ public class SyncServiceRpc {
         stub = LegacySyncGrpc.newBlockingStub(grpcChannelManager.getChannelSync());
     }
 
-    public void saveProjectSync(Long projectId) {
-        SyncInput.Builder request = SyncInput.newBuilder();
-        request.setProjectId(projectId.intValue());
-        stub.syncLegacy(request.build());
-    }
-
-    public void advanceFailedScreeningSubmissionSync(Long projectId, Long submissionId, boolean postMortemDeleted) {
+    public void saveProjectSync(Long projectId, boolean statusHasChanged, boolean categoryHasChanged,
+            boolean externalRefIdHasChanged, boolean directProjectIdHasChanged, boolean phaseUpdated,
+            boolean resourceUpdated, boolean prizeUpdated, boolean submissionUpdated) {
         SyncInput.Builder request = getSyncInput(projectId);
-        request.addUpdatedTables(getSubmissionTable(Arrays.asList(submissionId)));
-        if (postMortemDeleted) {
-            request.addUpdatedTables(getResourceInfoTable(null));
-            request.addUpdatedTables(getResourceSubmissionTable(null));
-            request.addUpdatedTables(getResourceTable(null));
+        if (statusHasChanged || categoryHasChanged || directProjectIdHasChanged) {
+            List<String> changedColumnNames = new ArrayList<>();
+            if (statusHasChanged) {
+                changedColumnNames.add("project_status_id");
+            }
+            if (categoryHasChanged) {
+                changedColumnNames.add("project_category_id");
+            }
+            if (directProjectIdHasChanged) {
+                changedColumnNames.add("tc_direct_project_id");
+            }
+            request.addUpdatedTables(getProjectTable(changedColumnNames));
+        }
+        if (externalRefIdHasChanged) {
+            request.addUpdatedTables(getProjectInfoTable(Arrays.asList(1L)));
+        }
+        if (phaseUpdated) {
             request.addUpdatedTables(getProjectPhaseTable(null));
             request.addUpdatedTables(getPhaseCriteriaTable(null));
+        }
+        if (resourceUpdated) {
+            request.addUpdatedTables(getResourceTable(null));
+            request.addUpdatedTables(getProjectPaymentTable(null));
+        }
+        if (prizeUpdated) {
+            request.addUpdatedTables(getPrizeTable(null));
+        }
+        if (submissionUpdated) {
+            request.addUpdatedTables(getSubmissionTable(null));
         }
         stub.syncLegacy(request.build());
     }
 
+    public void advanceFailedScreeningSubmissionSync(Long projectId, Long submissionId, boolean postMortemDeleted) {
+        if (postMortemDeleted) {
+            SyncInput.Builder request = getSyncInput(projectId);
+            request.addUpdatedTables(getProjectPhaseTable(null));
+            stub.syncLegacy(request.build());
+        }
+    }
+
     public void advanceFailedCheckpointScreeningSubmissionSync(Long projectId, Long submissionId) {
-        SyncInput.Builder request = getSyncInput(projectId);
-        request.addUpdatedTables(getSubmissionTable(Arrays.asList(submissionId)));
-        stub.syncLegacy(request.build());
+
     }
 
     public void manageProjectSync(Long projectId, boolean isPhasesUpdated, List<Long> resourceIds) {
@@ -63,25 +88,18 @@ public class SyncServiceRpc {
     }
 
     public void saveReviewPaymentsSync(Long projectId) {
-        SyncInput.Builder request = getSyncInput(projectId);
-        request.addUpdatedTables(getProjectPaymentTable(null));
-        request.addUpdatedTables(getProjectPaymentAdjustmentTable(projectId));
-        stub.syncLegacy(request.build());
+
     }
 
     public void SaveProjectPaymentsSync(Long projectId, List<Long> resourceIds) {
         SyncInput.Builder request = getSyncInput(projectId);
         request.addUpdatedTables(getProjectPaymentTable(null));
-        request.addUpdatedTables(getResourceInfoTable(resourceIds));
-        request.addUpdatedTables(getProjectResultTable(projectId));
         stub.syncLegacy(request.build());
     }
 
     public void SaveReviewSync(Long projectId) {
         SyncInput.Builder request = getSyncInput(projectId);
         request.addUpdatedTables(getSubmissionTable(null));
-        request.addUpdatedTables(getProjectPaymentTable(null));
-        request.addUpdatedTables(getProjectResultTable(projectId));
         stub.syncLegacy(request.build());
     }
 
@@ -89,21 +107,27 @@ public class SyncServiceRpc {
         return SyncInput.newBuilder().setProjectId(projectId.intValue());
     }
 
+    private Table getProjectTable(List<String> columnNames) {
+        Table.Builder table = Table.newBuilder().setTable("project").setPrimaryKey("COLUMNS");
+        addProperties(table, columnNames);
+        return table.build();
+    }
+
+    private Table getProjectInfoTable(List<Long> projectInfoTypeIds) {
+        Table.Builder table = Table.newBuilder().setTable("project_info").setPrimaryKey("project_info_type_id");
+        addValues(table, projectInfoTypeIds);
+        return table.build();
+    }
+
+    private Table getPrizeTable(List<Long> prizeIds) {
+        Table.Builder table = Table.newBuilder().setTable("prize").setPrimaryKey("prizeId");
+        addValues(table, prizeIds);
+        return table.build();
+    }
+
     private Table getSubmissionTable(List<Long> submissionIds) {
         Table.Builder table = Table.newBuilder().setTable("submission").setPrimaryKey("submission_id");
         addValues(table, submissionIds);
-        return table.build();
-    }
-
-    private Table getResourceInfoTable(List<Long> resourceIds) {
-        Table.Builder table = Table.newBuilder().setTable("resource_info").setPrimaryKey("resource_id");
-        addValues(table, resourceIds);
-        return table.build();
-    }
-
-    private Table getResourceSubmissionTable(List<Long> resourceIds) {
-        Table.Builder table = Table.newBuilder().setTable("resource_submission").setPrimaryKey("resource_id");
-        addValues(table, resourceIds);
         return table.build();
     }
 
@@ -125,31 +149,22 @@ public class SyncServiceRpc {
         return table.build();
     }
 
-    private Table getProjectPaymentAdjustmentTable(Long projectId) {
-        Table.Builder table = Table.newBuilder().setTable("project_payment_adjustment").setPrimaryKey("project_id");
-        if (projectId != null) {
-            table.addValue(projectId.toString());
-        }
-        return table.build();
-    }
-
     private Table getProjectPaymentTable(List<Long> projectPaymentIds) {
         Table.Builder table = Table.newBuilder().setTable("project_payment").setPrimaryKey("project_payment_id");
         addValues(table, projectPaymentIds);
         return table.build();
     }
 
-    private Table getProjectResultTable(Long projectId) {
-        Table.Builder table = Table.newBuilder().setTable("project_result").setPrimaryKey("project_id");
-        if (projectId != null) {
-            table.addValue(projectId.toString());
-        }
-        return table.build();
-    }
-
     private void addValues(Table.Builder builder, List<Long> values) {
         if (values != null) {
             for (Long value : values)
+                builder.addValue(value.toString());
+        }
+    }
+
+    private void addProperties(Table.Builder builder, List<String> values) {
+        if (values != null) {
+            for (String value : values)
                 builder.addValue(value.toString());
         }
     }
